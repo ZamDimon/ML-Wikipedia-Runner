2e4a3172d852afcb35094b9c25a4e0cedb2607f0
Dmytro Zakharov <zamdmytro@gmail.com>
2022-07-06 01:55:36 +0300

    Added interpolation function

--- internal/model/helper.py
+++ internal/model/helper.py
@@ -1,3 +1,5 @@
+import math
+
 from internal.dataset_generator import page_info
 
 
@@ -15,4 +17,17 @@ def get_input(title1, title2):
         result.append(word)
 
     return result
+
+
+# Namely, it is a modificated sigmoid: 2*sigmoid(x)-1
+def sigmoid(x):
+    return (1 - math.exp(-x)) / (1 + math.exp(-x))
+
+
+# Interpolates two inputs according to the following rule:
+# 1 and 1 give 1
+# 1 and 0 or 0 and 1 give -1
+# 0 and 0 give 0
+def interpolate(x, y):
+    return (sigmoid(x)*sigmoid(y)) - math.fabs(sigmoid(x) - sigmoid(y))
 
