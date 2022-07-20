6d32a303b898f30e216462ee93fad898d95cdab7
kartu72 <davyd.school@gmail.com>
2022-07-20 14:03:49 +0200

    my changes

--- internal/model/predictions.py
+++ internal/model/predictions.py
@@ -1,10 +1,16 @@
+import pickle
+import xgboost as xgb
+import sys
+from internal.dataset_generator import page_info
 def predict(title1, title2):
-    model_path = '0001.model'
-    tfidf_path = "tfidf.pkl"
+    print(sys.path)
+    model_path = '../internal/model/0001.model'
+    tfidf_path = '../internal/model/tfidf.pkl'
     bst = xgb.Booster({'nthread': 4})  # init model
-    bst.load_model('0001.model')
-    tfidf = pickle.load(open("tfidf.pkl",'rb'))
+    bst.load_model(model_path)
+    tfidf = pickle.load(open(tfidf_path,'rb'))
     #preprocessing
+    
     predict_q1_tfidf = tfidf.transform(page_info.get(title1))
     predict_q2_tfidf = tfidf.transform(page_info.get(title2))
     X_pred = xgb.DMatrix(abs(predict_q1_tfidf - predict_q2_tfidf))
