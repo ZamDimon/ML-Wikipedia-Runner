fdd8861a1a2ed255d1e989b75d76e3ace5ddf13d
kartu72 <davyd.school@gmail.com>
2022-07-21 16:50:08 +0200

    message

--- internal/model/predictions.py
+++ internal/model/predictions.py
@@ -3,14 +3,15 @@ import xgboost as xgb
 import sys
 from internal.dataset_generator import page_info
 
-model_path = '../internal/model/0001.model'
-tfidf_path = '../internal/model/tfidf.pkl'
+model_path = '../internal/model/0003.model'
+tfidf_path = '../internal/model/0003.pkl'
 bst = xgb.Booster({'nthread': 4})  # init model
 bst.load_model(model_path)
 tfidf = pickle.load(open(tfidf_path, 'rb'))
 
 
 def get_page_predict(title):
+    print(page_info.get(title))
     return tfidf.transform(page_info.get(title))
 
 
